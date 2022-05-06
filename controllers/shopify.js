9e928e6348f95cd820823726249e0d2576bccc18
Pamsar <99356910+thepythondevelopers@users.noreply.github.com>
2022-05-06 17:48:45 +0530

    Filter

--- controllers/shopify.js
+++ controllers/shopify.js
@@ -8,118 +8,10 @@ const ProductVariant = db.productVariant;
 const Customer = db.customer;
 const ShopifyToken = require('shopify-token');
 var pluck = require('arr-pluck');
+const moment= require('moment') 
 //const { json } = require('body-parser');
 
 const Op = db.Sequelize.Op;
-exports.getCustomer = (req,res) =>{
-  page_info = req.body.page_info;
-  const id = req.params.integration_id;
-  Integration.findByPk(id)
-    .then(data => {
-      if (data) {
-        const shopify = new Shopify({
-          shopName: data.domain,
-          accessToken: data.access_token
-        });
-        let params = { limit: process.env.PAGINATION_LIMIT,
-          page_info: page_info};
-        shopify.customer
-        .list(params)
-        .then((customers) => res.json(
-          {data : customers,
-          nextPage : customers.nextPageParameters,
-        prevPage : customers.previousPageParameters }
-          ))
-        .catch((err) => res.json(err));
-      } else {
-        res.status(404).send({
-          message: `Cannot connect shopify with id=${id}.`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error retrieving shopify account with id=" + id
-      });
-    });
- 
-    
-}
-
-exports.getOrder = (req,res) =>{
-  page_info = req.body.page_info;
-  const id = req.params.integration_id;
-  Integration.findByPk(id)
-    .then(data => {
-      if (data) {
-        const shopify = new Shopify({
-          shopName: data.domain,
-          accessToken: data.access_token
-        });
-        let params = { limit: process.env.PAGINATION_LIMIT,
-          page_info: page_info};
-        shopify.order
-        .list(params)
-        .then((orders) => res.json(
-          {data : orders,
-          nextPage : orders.nextPageParameters,
-        prevPage : orders.previousPageParameters }
-          ))
-        .catch((err) => res.json(err));
-      } else {
-        res.status(404).send({
-          message: `Cannot connect shopify with id=${id}.`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error retrieving shopify account with id=" + id
-      });
-    });
-    
-    
-}
-
-exports.getProduct = (req,res) =>{
-  page_info = req.body.page_info;
-  const id = req.params.integration_id;
-  Integration.findByPk(id)
-    .then(data => {
-      if (data) {
-        const shopify = new Shopify({
-          shopName: data.domain,
-          accessToken: data.access_token
-        });
-        let params = { limit: process.env.PAGINATION_LIMIT,
-          page_info: page_info};
-        
-        shopify.product
-        .list(params)
-        .then((products) => res.json(
-          {data : products,
-          nextPage : products.nextPageParameters,
-        prevPage : products.previousPageParameters }
-          ))
-        .catch((err) => res.json(err));
-        
-      } else {
-        res.status(404).send({
-          message: `Cannot connect shopify with id=${id}.`
-        });
-      }
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: "Error retrieving shopify account with id=" + id
-      });
-    });
-    
-   
-}
-
-
-
 
 exports.getSingleProduct = async (req,res) =>{
   const id = req.params.product_id;
@@ -140,24 +32,11 @@ exports.getSingleCustomer = async (req,res) =>{
   return res.json({data:result});
 }
 
-exports.getAllCustomer = async (req,res) =>{
- 
-  result = await Customer.findAll({
-    where: {store_id : 0}
-  })
-  return res.json({data:result});
-}
-exports.getAllProduct = async (req,res) =>{
-  
-  result = await Product.findAll({
-    where: {store_id : 0}
-  })
-  return res.json({data:result});
-}
+
 
 
 exports.getProductAccordingtoStore = async (req,res) =>{
-  
+  try {  
   store_id =req.body.store_id!=null ? req.body.store_id : [];
   if(store_id==0){
     store_id = await Integration.findAll({
@@ -169,32 +48,66 @@ exports.getProductAccordingtoStore = async (req,res) =>{
     })
     store_id = pluck(store_id, 'store_id');
   }
-//   const startedDate = "2022-04-24 01:01:01";
-// const endDate = "2022-04-28 23:23:59";
+   
+
+
+   
+   
 const search_string = req.body.search_string!=null ? req.body.search_string : "";
- 
+if(req.body.startedDate!=null && req.body.endDate!=null ){
+  
+  const startedDate = new Date(req.body.startedDate);
+  const endDate = new Date(req.body.endDate);
+  endDate.setDate(endDate.getDate() + 1);
+
+
   result = await Product.findAll({
     where: {store_id : {
       [Op.in]: store_id  
     },  [Op.or]: [
       { store_id: { [Op.like]: `%${search_string}%` } },
       { product_type: { [Op.like]: `%${search_string}%` } },
-      { product_type: { [Op.like]: `%${search_string}%` } }
+      { product_type: { [Op.like]: `%${search_string}%` } },
+      
     ],
     
-  //   created_at: {
-  //     [Op.between]: [startedDate, endDate]
-  // }
+        created_at: {
+       [Op.between]: [startedDate, endDate]
+   }
   
   },
     include: [{
         model: ProductVariant
     }]
   })
+}else{
+  result = await Product.findAll({
+    where: {store_id : {
+      [Op.in]: store_id  
+    },  [Op.or]: [
+      { store_id: { [Op.like]: `%${search_string}%` } },
+      { product_type: { [Op.like]: `%${search_string}%` } },
+      { product_type: { [Op.like]: `%${search_string}%` } },
+      
+    ],
+    
+  },
+    include: [{
+        model: ProductVariant
+    }]
+  })
+} 
   return res.json({data:result});
+}  catch (err) {
+    return res.status(401).send({
+      message : "Something Went Wrong",
+      error :err
+    });
+  }
 }
 
 exports.getCustomerAccordingtoStore = async (req,res) =>{
+  try {
   store_id =req.body.store_id!=null ? req.body.store_id : [];
   if(store_id==0){
     store_id = await Integration.findAll({
@@ -207,91 +120,42 @@ exports.getCustomerAccordingtoStore = async (req,res) =>{
     store_id = pluck(store_id, 'store_id');
   }
   const search_string = req.body.search_string!=null ? req.body.search_string : "";
-  result = await Customer.findAll({
-    where: {store_id : {
-      [Op.in]: store_id 
-    },[Op.or]: [
-      { store_id: { [Op.like]: `%${search_string}%` } },
-      { product_type: { [Op.like]: `%${search_string}%` } },
-      { product_type: { [Op.like]: `%${search_string}%` } }
-    ]}
-  })
+  if(req.body.startedDate!=null && req.body.endDate!=null ){
+  
+    const startedDate = new Date(req.body.startedDate);
+    const endDate = new Date(req.body.endDate);
+    endDate.setDate(endDate.getDate() + 1);
+    result = await Customer.findAll({
+      where: {store_id : {
+        [Op.in]: store_id 
+      },[Op.or]: [
+        { store_id: { [Op.like]: `%${search_string}%` } },
+        { first_name: { [Op.like]: `%${search_string}%` } },
+        { last_name: { [Op.like]: `%${search_string}%` } }
+      ],
+      created_at: {
+        [Op.between]: [startedDate, endDate]
+    }
+    }
+    })
+  }else{
+    result = await Customer.findAll({
+      where: {store_id : {
+        [Op.in]: store_id 
+      },[Op.or]: [
+        { store_id: { [Op.like]: `%${search_string}%` } },
+        { first_name: { [Op.like]: `%${search_string}%` } },
+        { last_name: { [Op.like]: `%${search_string}%` } }
+      ]}
+    })
+  }  
+  
   return res.json({data:result});
 }
-
-exports.getShopify = (req,res) =>{
-  const shopifyToken1 = new ShopifyToken({
-    sharedSecret: '8b512161192bcabeb8924111a66a394b',
-    redirectUri: 'http://localhost:8080/shopify/api/callback',
-    apiKey: '5342f252555085c5f295c11b0b904bd3'
+catch (err) {
+  return res.status(401).send({
+    message : "Something Went Wrong",
+    error :err
   });
-  const nonce1 = shopifyToken1.generateNonce();
-
-console.log(nonce1);
-const url1 = shopifyToken1.generateAuthUrl('testing');
-return res.json(url1);
-const ok = shopifyToken.verifyHmac(url1);
-
-console.log(ok);
-
- return res.json('r');
-  const shopify = new Shopify({
-    shopName: 'pamsar-test',
-    // apiKey: 'cb2ed50cf1170e685d63912d8c363a92',
-    accessToken: '42b26ad0494bdbc6e5b6973542622de4'
-  });
-
-  shopify.order
-  .list({ limit: 5 })
-  .then((orders) => console.log(orders))
-  .catch((err) => console.error(err));
-  return res.json('Okay');
-  // config={
-  //     shopName: 'pamsar.myshopify.com',
-  //     apiKey: 'cf0bbf9bc3e2416ad13f44496a05c9cc',
-  //     password: '35b41d6bacd5a4d2b3f40ef89eabf1af'
-  //   }
-  // var Shopify = new shopifyAPI(config), // You need to pass in your config here
-  // query_params = req.query;
-
-// Shopify.exchange_temporary_token(query_params, function(err, data){
-  // This will return successful if the request was authentic from Shopify
-  // Otherwise err will be non-null.
-  // The module will automatically update your config with the new access token
-  // It is also available here as data['access_token']
-  // console.log(data);
-// });
-  // const shopify = new Shopify({
-  //   shopName: 'pamsar.myshopify.com',
-  //   apiKey: 'cf0bbf9bc3e2416ad13f44496a05c9cc',
-  //   password: '35b41d6bacd5a4d2b3f40ef89eabf1af'
-  // });
-//   const shopify = new Shopify({
-//     shopName: 'pamsar',
-//     accessToken: '9e3191f8054b550d76f4b883c1135a2a'
-//   });
-  
-//   shopify.product.list()
-// .then(products =>  res.send(products))
-// .catch(err =>  console.log(err));
-// const shopifyToken = new ShopifyToken({
-//   sharedSecret: '8ceb18e8ca581aee7cad1ddd3991610b',
-//   redirectUri: 'http://localhost:8080/callback',
-//   apiKey: 'cf0bbf9bc3e2416ad13f44496a05c9cc'
-// });
-// const nonce = shopifyToken.generateNonce();
-
-// console.log(nonce);
-const shopifyToken = new ShopifyToken({
-  sharedSecret: 'b520bc1bfe3ec7acf502d11f60b9ac37',
-  redirectUri: 'http://localhost:8000/auth/callback',
-  apiKey: 'cb2ed50cf1170e685d63912d8c363a92'
-});
-const nonce = shopifyToken.generateNonce();
-
-
-// const url = shopifyToken.generateAuthUrl('dolciumi');
-const url =shopifyToken.generateAuthUrl('pamsar', '', nonce, '')
-console.log(url);
-return res.json(url);
+}
 }
--- routes/shopify.js
+++ routes/shopify.js
@@ -1,18 +1,13 @@
 var express = require('express')
 var router = express.Router()
 
-const {getCustomer,getOrder,getProduct,getShopify,getSingleProduct,getSingleCustomer,getAllCustomer,getAllProduct,getProductAccordingtoStore,getCustomerAccordingtoStore} = require("../controllers/shopify");
+const {getSingleProduct,getSingleCustomer,getProductAccordingtoStore,getCustomerAccordingtoStore} = require("../controllers/shopify");
 const {verifyToken,isAccountCheck,roleCheck} = require("../controllers/auth");
 
-router.get("/customer/:integration_id",getCustomer);
-router.get("/order/:integration_id",getOrder);
-router.get("/product/:integration_id",getProduct);
 
-router.get("/get-shopify",getShopify);
 router.get("/get-single-shopify/:product_id",verifyToken,getSingleProduct);
 router.get("/get-single-customer/:customer_id",verifyToken,getSingleCustomer);
-//router.get("/get-all-customer",verifyToken,getAllCustomer);
-//router.get("/get-all-product",verifyToken,getAllProduct);
+
 
 router.get("/get-all-product-store",verifyToken,isAccountCheck,getProductAccordingtoStore);
 router.get("/get-all-customer-store",verifyToken,getCustomerAccordingtoStore);
