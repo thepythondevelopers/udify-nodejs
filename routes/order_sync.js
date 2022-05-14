38f57a8958e0e229fd948b763aaae72dcbaafb55
Pamsar <99356910+thepythondevelopers@users.noreply.github.com>
2022-05-15 00:40:28 +0530

    Route Chnage

--- routes/order_sync.js
+++ routes/order_sync.js
@@ -8,7 +8,7 @@ const {syncOrder,getOrderAccordingtoStore,getSingleOrder} = require("../controll
 const {verifyToken,isAccountCheck,roleCheck} = require("../controllers/auth");
 
 
-router.get("/shopify-order-sync/:integration_id",verifyToken,syncOrder);
+router.get("/sync-order/:integration_id",verifyToken,syncOrder);
 
 router.post("/get-all-order-store",verifyToken,getOrderAccordingtoStore);
 router.get("/get-single-order/:order_id",verifyToken,getSingleOrder);
